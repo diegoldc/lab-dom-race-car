ce34a61401fea9d6aee32414af4533f4fe7ec762
Diego Lázaro <dlazarodecastro@gmail.com>
2024-10-07 18:05:09 +0200

    primeros pasos

--- js/game.js
+++ js/game.js
@@ -1,3 +1,41 @@
 class Game {
     // code to be added
+    constructor() {
+        this.startScreen = document.querySelector("#game-intro")
+        this.gameScreen = document.querySelector("#game-screen")
+        this.gameEndScreen = document.querySelector("#game-end")
+        this.player = null
+        this.height = 600
+        this.width = 500
+        this.obstacles = []
+        this.score = 0
+        this.lives = 3
+        this.gameIsOver = false
+        this.gameIntervalId
+        this.gameLoopFrecuency = 1000 / 60
+    }
+
+    start() {
+        this.gameScreen.style.height = `${this.height}px`
+        this.gameScreen.style.width = `${this.width}px`
+
+        this.startScreen.style.display = "none"
+        this.gameScreen.style.display = "block"
+
+        this.gameIntervalId = setInterval(() => {
+            gameLoop()
+        }, this.gameLoopFrecuency)
+    }
+
+    gameLoop() {
+        update()
+
+        if (this.gameIsOver === true) {
+            clearInterval(this.gameIntervalId)
+        }
+    }
+
+    update() {
+
+    }
 }
--- js/script.js
+++ js/script.js
@@ -8,5 +8,8 @@ window.onload = function () {
 
   function startGame() {
     console.log("start game");
+    game = new Game()
+
+    game.start()
   }
 };
